0a63e6a7d9936da3aac2549ef2e4ddff4c826eff
SlaviHristev <Slavihristev97@gmail.com>
2024-04-14 19:41:07 +0300

    Made my sidebar component and used framer-motion for it

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import Navbar from './components/navbar/Navbar';
 
 const App = () => {
   return <div>
-    <section><Navbar/></section>
+    <section id='Homepage'><Navbar/></section>
     <section>Parallax</section>
-    <section>Services</section>
+    <section id='Services'>Services</section>
     <section>Parallax</section>
-    <section>Portfolio1</section>
+    <section id='Portfolio'>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
-    <section>Contact</section>
+    <section id='Contact'>Contact</section>
 
   </div>;
 };
