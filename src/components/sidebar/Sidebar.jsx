88b7cc7df05382bb8c80706042ca78eff69e7c0f
SlaviHristev <Slavihristev97@gmail.com>
2024-04-14 11:54:46 +0300

    Started using framer-motion into navbar

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './sidebar.scss'
+
+
+
+const Sidebar = () => {
+
+    return(
+        <div className="sidebar">
+
+        </div>
+    )
+}
+
+export default Sidebar
