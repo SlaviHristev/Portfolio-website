36fa11965cd3311786142fe0a3c5cc24558b7685
SlaviHristev <Slavihristev97@gmail.com>
2024-04-21 12:33:23 +0300

    Started making the portfolio sections

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './app.scss'
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 import Parallax from './components/parallax/Parallax';
+import Portfolio from './components/portfolio/Portfolio';
 import Services from './components/services/Services';
 
 const App = () => {
@@ -13,8 +14,7 @@ const App = () => {
     <section id='Services'><Parallax type="services"/></section>
     <section><Services/></section>
     <section id='Portfolio'><Parallax type="portfolio"/></section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
+    <Portfolio/>
     <section id='Contact'>Contact</section>
 
   </div>;
